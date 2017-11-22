30c322860c1e760ddd0251759ef673e28d38f85f
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-11-22 22:11:28 +0800

    Add project files.

---
+++ AspNetCoreWithWebSockets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreWithWebSockets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
