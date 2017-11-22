e07b275a75ba4c1d5a9df4b035818d946c14e5de
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-11-22 22:55:30 +0800

    update controller.cs

--- AspNetCoreWithWebSockets/Controllers/HomeController.cs
+++ AspNetCoreWithWebSockets/Controllers/HomeController.cs
@@ -14,21 +14,7 @@ namespace AspNetCoreWithWebSockets.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
